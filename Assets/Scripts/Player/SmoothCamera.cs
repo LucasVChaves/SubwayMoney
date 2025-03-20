70020980f43a74430ef2bdef9e4d896774fcb4e5
Lucas <lucasvchaves@gmail.com>
2025-03-20 20:13:06 -0300

    fix: Aumenta a distancia Z de spawn do trem e melhora o angulo da camera do player

--- Assets/Scripts/Player/SmoothCamera.cs
+++ Assets/Scripts/Player/SmoothCamera.cs
@@ -4,6 +4,11 @@ public class SmoothCamera : MonoBehaviour {
     public Transform player;
     public float smoothSpeed = 0.125f;
     public Vector3 offset;
+    public Vector3 rotation;
+
+    void Start() {
+        transform.rotation = Quaternion.Euler(rotation);
+    }
 
     void Update() {
         Vector3 desiredPosition = player.position + offset;
